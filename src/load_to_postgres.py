7970a38ff1cd928f8f64769c44a6396b279196f5
Saif Ali <saifeeali512@yahoo.com>
2026-06-11 00:08:59 +0100

    Dockerized PostgreSQL and completed containerized deployment

--- src/load_to_postgres.py
+++ src/load_to_postgres.py
@@ -2,11 +2,17 @@ import json
 import psycopg2
 
 # PostgreSQL connection
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 conn = psycopg2.connect(
-    host="localhost",
-    database="rna_curator_ai",
-    user="postgres",
-    password="hamlog512"
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
 )
 
 cursor = conn.cursor()
